f07a0a202e179b27368bb53de84318152f95b96e
Boris MBARGA <bmbarga@e3r11p1.42.fr>
2014-04-27 20:28:36 +0200

    fg

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmbarga <bmbarga@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2013/12/02 16:36:21 by bmbarga           #+#    #+#             */
+/*   Updated: 2014/04/27 19:00:19 by bmbarga          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define MY_EOF -1
+# define BUFF_SIZE 10
+
+int		get_next_line(int const fd, char **line);
+enum	e_str
+{
+	TMP,
+	STR,
+	BUFF
+};
+#endif
